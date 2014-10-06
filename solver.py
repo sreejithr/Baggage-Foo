e73442d30661d5550036859a57621b9611bcfa80
Sreejith <sreejith.r44@gmail.com>
2014-10-07 01:04:17 +0530

    Started with basic structure for the BaggageState class

---
+++ solver.py
@@ -0,0 +1,11 @@
+"""
+Baggage Bin Problem Solver
+--------------------------
+@author: Sreejith R
+"""
+class BaggageState(object):
+    """
+    Represents state of baggage bins.
+    """
+    pass
+
