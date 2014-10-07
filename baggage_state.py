4419b19c4d335c6597a0b8299c56ea742e71dd82
Sreejith <sreejith.r44@gmail.com>
2014-10-07 12:06:22 +0530

    Added __iter__ and __getitem__ to BaggageState to make it emulate a list structure which returns elements 2 at a time

--- baggage_state.py
+++ baggage_state.py
@@ -11,3 +11,17 @@ class BaggageState(object):
         self.state = state
         self.bins_per_cart = bins_per_cart
         self.empty_token = empty_space_token
+    def __getitem__(self, key):
+        if type(key) is not int or key < 0:
+            raise
+
+        if len(state[key:key+self.bins_per_cart]) < 2:
+            raise
+        else:
+            return state[key:key+self.bins_per_cart]
+
+    def __iter__(self):
+        i = 0
+        while i < len(self.state):
+            yield self.state[i:i+self.bins_per_cart]
+            i += self.bins_per_cart
