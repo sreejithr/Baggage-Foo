ff49da1b8592e8f47318197eb7fedcf8c32239e4
Sreejith <sreejith.r44@gmail.com>
2014-10-07 12:19:47 +0530

    Made BaggageManager class

---
+++ manager.py
@@ -0,0 +1,11 @@
+"""
+BaggageManager
+--------------
+@author: Sreejith R
+"""
+from .state import BaggageState
+
+class BaggageManager(object):
+    """
+    BaggageManager is delegated with all the logic which moves the baggage bins.
+    """
