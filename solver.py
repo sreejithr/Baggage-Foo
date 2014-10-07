4a77a4887bb78482f654291f12e2d29ea2b32624
Sreejith <sreejith.r44@gmail.com>
2014-10-07 17:45:41 +0530

    Started writing A* search for finding optimum moves

--- solver.py
+++ solver.py
@@ -3,6 +3,22 @@ Baggage Bin Problem using A* Algorithm
 --------------------------------------
 @author: Sreejith R
 """
+from heapq import heappush
+
 from .state import BaggageState
+from .manager import BaggageManager
+
+EMPTY_SPACE_TOKEN = '*'
+
+
+class Astar(object):
+    def __init__(self, start, goal):
+        self.start = start
+        self.goal = goal
+        self.baggage_manager = BaggageManager()
+
+        self.to_visit = []
+        self.cost_so_far = {self.start: 0}
+        self.came_from = {self.start: None}
     pass
 
