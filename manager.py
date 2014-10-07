895f883d7a17177ce2c2ba67a1136960605e440f
Sreejith <sreejith.r44@gmail.com>
2014-10-07 12:20:53 +0530

    Wrote logic to generate all the states possible with single movement given a state

--- manager.py
+++ manager.py
@@ -9,3 +9,23 @@ class BaggageManager(object):
     """
     BaggageManager is delegated with all the logic which moves the baggage bins.
     """
+    def _is_valid_src(self, state, index):
+        return not state.is_empty_at_index(index)
+
+    def _is_valid_dest(self, state, index):
+        return state.is_empty_at_index(index)
+
+    def next_possible_states(self, state):
+        # For each node, generate all possible single moves
+        def make_new_state(state, src, dest):
+            if self._is_valid_dest(state, dest):
+                new_state = BaggageState(state, state.bins_per_cart)
+                new_state.replace(src, dest)
+                return new_state
+            return False
+
+        for src, _ in enumerate(state):
+            if self._is_valid_src(state, src):
+                for dest, _ in enumerate(state):
+                    new_state = make_new_state(state, src, dest)
+                    yield new_state
